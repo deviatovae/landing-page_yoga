f21cf8a61e88378ddd1593e7a6d16f2bc346f78a
Alena Deviatova <www.fidgetl@gmail.com>
2022-06-05 18:14:09 +0300

    added new block "courses"

--- js/script.js
+++ js/script.js
@@ -3,5 +3,20 @@ $(document).ready(function (){
         arrows: true,
         adaptiveHeight: true,
         slidesToShow: 2,
+        variableWidth: true,
+        infinite: true,
+        speed: 500,
+        draggable: true,
+        slidesToScroll: 2,
+    });
+    $('.courses__slider').slick({
+        arrows: true,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        variableWidth: true,
+        infinite: true,
+        speed: 500,
+        draggable: true,
+        slidesToScroll: 1,
     });
 })
